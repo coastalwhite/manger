a02f3aeec686449fc9280cdf1f523be1f9e252ac
Gijs Burghoorn <g.burghoorn@gmail.com>
2020-12-29 14:55:44 +0100

    Added documentation for chars module

--- src/chars.rs
+++ src/chars.rs
@@ -27,47 +27,35 @@ impl Consumable for char {
     }
 }
 
-macro_rules! char_impl {
-    ( $typename:ident => $char:literal ) => {
-        impl $crate::Consumable for $typename {
-            fn consume_from(s: &str) -> Result<(Self, &str), $crate::error::ConsumeError> {
-                use $crate::error::ConsumeError;
-                use $crate::error::ConsumeErrorType::*;
+macro_rules! declare_ascii {
+    ( $( $struct_name:ident => $char:literal ),+ ) => {
+        $(
+            /// Chars that represents the respective utf-8 character.
+            #[derive(Debug, PartialEq)]
+            pub struct $struct_name;
 
-                match s.chars().next() {
-                    Some(token) => {
-                        if (token == $char) {
-                            Ok(($typename, utf8_slice::from(s, 1)))
-                        } else {
-                            Err(ConsumeError::new_with(UnexpectedToken { index: 0, token }))
-                        }
-                    }
-                    _ => Err(ConsumeError::new_with(InsufficientTokens { index: 0 })),
+            impl From<$struct_name> for char {
+                fn from(_: $struct_name) -> char {
+                    $char
                 }
             }
-        }
+
+            impl From<$struct_name> for u8 {
+                fn from(_: $struct_name) -> u8 {
+                    $char as u8
+                }
+            }
+
+            crate::consume_struct!(
+                $struct_name => [
+                    > $char;
+                ]
+            );
+        )+
     };
 }
 
-#[macro_export]
-macro_rules! manger_char {
-    ( $typename:ident => $char:literal ) => {
-        struct $typename;
-        char_impl! ( $typename => $char );
-    };
-    ( @pub $typename:ident => $char:literal ) => {
-        pub struct $typename;
-        char_impl! ( $typename => $char );
-    };
-}
-
-macro_rules! declare_chars {
-    ( $( $typename:ident => $char:literal ),* ) => {
-        $( manger_char!( @pub $typename => $char ); )*
-    };
-}
-
-declare_chars![
+declare_ascii![
     Space => ' ',
     Tab => '\t',
     NewLine => '\n',
@@ -108,100 +96,108 @@ declare_chars![
     Equals => '='
 ];
 
-pub mod nums {
-    declare_chars![
-    Zero => '0',
-    One => '1',
-    Two => '2',
-    Three => '3',
-    Four => '4',
-    Five => '5',
-    Six => '6',
-    Seven => '7',
-    Eight => '8',
-    Nine => '9'
+/// _Structs_ to consume __decimals digits__.
+pub mod num {
+    declare_ascii![
+        Zero => '0',
+        One => '1',
+        Two => '2',
+        Three => '3',
+        Four => '4',
+        Five => '5',
+        Six => '6',
+        Seven => '7',
+        Eight => '8',
+        Nine => '9'
     ];
 }
 
-pub mod alphabet {
-    use either::Either;
+/// _Enums_ to consume letters for the __Latin Alphabet__.
+pub mod alpha {
+    macro_rules! letter {
+        ( $( $letter:ident => [ $lower_char:literal, $upper_char:literal ] ),* ) => {
+            $(
+                /// `Enum` for consuming the corresponding case-independent letter.
+                ///
+                /// Upper-case letters gets turned into `Uppercase` and lower-case letters
+                /// gets turned into `Lowercase`.
+                #[derive(Debug, PartialEq)]
+                pub enum $letter {
+                    /// Lowercase variant of corresponding letter.
+                    Lowercase,
 
-    macro_rules! letter_either {
-        ( $( $letter:ident ),* ) => {
-            $( pub type $letter = Either<lower::$letter, upper::$letter>; )*
+                    /// Uppercase variant of corresponding letter.
+                    Uppercase,
+                }
+
+                impl From<$letter> for char {
+                    fn from(letter: $letter) -> char {
+                        use $letter::*;
+
+                        match letter {
+                            Lowercase => char::from( lower::$letter ),
+                            Uppercase => char::from( upper::$letter ),
+                        }
+                    }
+                }
+
+                impl From<$letter> for u8 {
+                    fn from(letter: $letter) -> u8 {
+                        use $letter::*;
+
+                        match letter {
+                            Lowercase => u8::from( lower::$letter ),
+                            Uppercase => u8::from( upper::$letter ),
+                        }
+                    }
+                }
+
+                crate::consume_enum!(
+                    $letter {
+                        Lowercase => [ : lower::$letter; ],
+                        Uppercase => [ : upper::$letter; ]
+                    }
+                );
+            )*
+
+            /// _Structs_ to consume lower-case letters for the __Latin Alphabet__.
+            pub mod lower {
+                declare_ascii!( $( $letter => $lower_char ),* );
+            }
+
+            /// _Structs_ to consume upper-case letters for the __Latin Alphabet__.
+            pub mod upper {
+                declare_ascii!( $( $letter => $upper_char ),* );
+            }
         };
     }
 
-    letter_either![A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z];
-
-    pub mod upper {
-        declare_chars![
-            A => 'A',
-            B => 'B',
-            C => 'C',
-            D => 'D',
-            E => 'E',
-            F => 'F',
-            G => 'G',
-            H => 'H',
-            I => 'I',
-            J => 'J',
-            K => 'K',
-            L => 'L',
-            M => 'M',
-            N => 'N',
-            O => 'O',
-            P => 'P',
-            Q => 'Q',
-            R => 'R',
-            S => 'S',
-            T => 'T',
-            U => 'U',
-            V => 'V',
-            W => 'W',
-            X => 'X',
-            Y => 'Y',
-            Z => 'Z'
-        ];
-    }
-
-    pub mod lower {
-        declare_chars![
-            A => 'a',
-            B => 'b',
-            C => 'c',
-            D => 'd',
-            E => 'e',
-            F => 'f',
-            G => 'g',
-            H => 'h',
-            I => 'i',
-            J => 'j',
-            K => 'k',
-            L => 'l',
-            M => 'm',
-            N => 'n',
-            O => 'o',
-            P => 'p',
-            Q => 'q',
-            R => 'r',
-            S => 's',
-            T => 't',
-            U => 'u',
-            V => 'v',
-            W => 'w',
-            X => 'x',
-            Y => 'y',
-            Z => 'z'
-        ];
-    }
+    letter![
+        A => [ 'a', 'A' ],
+        B => [ 'b', 'B' ],
+        C => [ 'c', 'C' ],
+        D => [ 'd', 'D' ],
+        E => [ 'e', 'E' ],
+        F => [ 'f', 'F' ],
+        G => [ 'g', 'G' ],
+        H => [ 'h', 'H' ],
+        I => [ 'i', 'I' ],
+        J => [ 'j', 'J' ],
+        K => [ 'k', 'K' ],
+        L => [ 'l', 'L' ],
+        M => [ 'm', 'M' ],
+        N => [ 'n', 'N' ],
+        O => [ 'o', 'O' ],
+        P => [ 'p', 'P' ],
+        Q => [ 'q', 'Q' ],
+        R => [ 'r', 'R' ],
+        S => [ 's', 'S' ],
+        T => [ 't', 'T' ],
+        U => [ 'u', 'U' ],
+        V => [ 'v', 'V' ],
+        W => [ 'w', 'W' ],
+        X => [ 'x', 'X' ],
+        Y => [ 'y', 'Y' ],
+        Z => [ 'z', 'Z' ]
+    ];
 }
-
-use crate::consume_struct;
-pub struct Whitespace;
-
-consume_struct!(
-    Whitespace => [
-        : char { |token: char| token.is_whitespace() };
-    ]
-);
