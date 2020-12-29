7a9d58f02a4cf2fb252a94e8100ad646851f0844
Gijs Burghoorn <g.burghoorn@gmail.com>
2020-12-29 16:35:46 +0100

    Renamed `Nothing` to `CatchAll` and added documentation for CatchAll.

--- src/common/nothing.rs
+++ src/common/nothing.rs
@@ -1,10 +1,13 @@
 use crate::consume_struct;
 
+/// A catch-all clause for consuming.
+///
+/// Most often used with Enums and with the `Either<L, R>` struct.
 #[derive(Debug, PartialEq)]
-pub struct Nothing;
+pub struct CatchAll;
 
 consume_struct!(
-    Nothing => [
+    CatchAll => [
         > "";
     ]
 );
